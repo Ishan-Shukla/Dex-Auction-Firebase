8924030805853b100c9be7ce98a1408270534796
Ishan Shukla <ishanshukla05@gmail.com>
2021-09-22 17:11:50 +0530

    navbar Locations

--- front-end/src/Components/NavBar/NavBar.js
+++ front-end/src/Components/NavBar/NavBar.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const [status, setStatus] = useState(0);
+  const location = useLocation();
+  useEffect(()=>{
+    const path = location.pathname;
+    if (path.includes("/MyAssets")) {
+      console.log("This is Path");
+    }else if (path.includes("/Market")) {
+      console.log("This is Market");
+    }else if (path.includes("/NFT")) {
+      console.log("This is NFT");
+    }
+  },[location])
+  console.log(location.pathname);
   return (
     <div className="fixed flex z-50 text-xl top-1/2 transform -rotate-90 -right-24 ">
-      <Link to="/MyAssets" className="flex-grow p-4">My Assets</Link>
-      <Link to="/Market" className="flex-grow p-4">MarketPlace</Link>
+      <div>
+        <Link to="/MyAssets" className="flex-grow p-4">
+          My Assets
+        </Link>
+      </div>
+      <div>
+        <Link to="/Market" className="flex-grow p-4">
+          MarketPlace
+        </Link>
+      </div>
     </div>
   );
 }
