f06ac89f03239ff6eeddfe39a719675f42123dbb
Ishan Shukla <ishanshukla05@gmail.com>
2021-10-22 16:12:52 +0530

    NavBar hiding issue fixed

--- front-end/src/Components/NavBar/NavBar.js
+++ front-end/src/Components/NavBar/NavBar.js
@@ -4,36 +4,50 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const [status, setStatus] = useState(0);
-  const [isShowing, setIsShowing] = useState(true);
+  const [loadingState, setLoadingState] = useState("not-loaded");
   const location = useLocation();
+
   useEffect(() => {
     const path = location.pathname;
     if (path.includes("/MyAssets")) {
-      console.log("This is Path");
+      console.log("This is MyAssets");
+      setStatus(1);
     } else if (path.includes("/Market")) {
       console.log("This is Market");
-    } else if (path.includes("/NFT")) {
-      console.log("This is NFT");
+      setStatus(2);
+    } else {
+      setStatus(0);
     }
+    setLoadingState("loaded");
   }, [location]);
-  console.log(location.pathname);
+
+  const changeStatus = () => {
+    setLoadingState("not-loaded");
+  };
+
+  const home = "-right-44";
+  const other = "-right-28";
+
   return (
-    <div className="h-26 fixed z-50 text-xl font-sans top-1/2 transform -rotate-90 -right-44 rounded-t-3xl pl-4 pr-4 pb-10 bg-blue-200 bg-opacity-10 border-t-8 border-l-2 border-r-2 border-gray-300 backdrop-filter backdrop-blur-md backdrop-brightness-70 transition duration-200 ease-in-out hover:-translate-x-10">
-      <div>
-        <div className="mt-2 mb-2 ml-6 mr-6 bg-gray-300 pt-1 rounded-full"></div>
-        <div className="flex flex-row mt-5">
-          <div>
+    <div className={`h-26 flex flex-col fixed z-50 text-xl top-1/2 ${status === 0 ? home : other} transform -rotate-90 rounded-t-3xl pl-4 pr-4 pb-10 bg-blue-200 bg-opacity-10 border-t-8 border-l-2 border-r-2 border-gray-300 backdrop-filter backdrop-blur-md backdrop-brightness-70 transition duration-200 ease-in-out hover:-translate-x-10`}>
+      <div className="mt-2 mb-2 ml-6 mr-6 bg-gray-300 pt-1 rounded-full"></div>
+      <div className="flex flex-row mt-2 mb-4">
+        {status === 0 || status === 2 ? (
+          <div className="mb-4">
             <Link to="/MyAssets" className="p-4">
               My Assets
             </Link>
           </div>
-          <div>
+        ) : null}
+        {status === 0 || status === 1 ? (
+          <div className="mb-2">
             <Link to="/Market" className="p-4">
               MarketPlace
             </Link>
           </div>
-        </div>
+        ) : null}
       </div>
     </div>
   );
 }
+
